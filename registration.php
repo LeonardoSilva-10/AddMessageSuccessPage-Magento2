b3c083d7d04e2bf4695c206725bb9a420738a220
leonardo.ferreira-10 <leonardo.ferreira@bistwobis.com.br>
2021-07-19 16:20:00 -0300

    :tada: Module Base

---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(
+      ComponentRegistrar::MODULE,
+      'LsBR_MessageAdditionalSuccessPage',
+      __DIR__
+  );
